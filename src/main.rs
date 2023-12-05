379984a3afc8b78b23bfcd9a3f23493fd27c8aa3
elton-cs <govada.elton@gmail.com>
2023-12-05 07:46:46 -0600

    Add save_block JSON function

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
 use std::fs;
-use std::fs::File;
 use std::io::{BufRead, BufReader};
+use std::fs::File;
+use std::io::Write;
 
-fn rename_json( old_name: &str, new_name: &str){
-    fs::rename(old_name, new_name)
-    .expect("Could not rename the file");
-}
+// fn rename_json( old_name: &str, new_name: &str){
+//     fs::rename(old_name, new_name)
+//     .expect("Could not rename the file");
+// }
 
 fn search_by_string_json(file_path: &str, start_string: &str) -> Vec<String> {
-    let file = File::open(file_path).expect("Failed to open file");
+    let file = fs::File::open(file_path).expect("Failed to open file");
     let reader = BufReader::new(file);
 
     let mut lines: Vec<String> = Vec::new();
@@ -31,15 +32,57 @@ fn search_by_string_json(file_path: &str, start_string: &str) -> Vec<String> {
     lines
 }
 
+fn save_block_json(file_path: &str, start_string: &str, end_string: &str) -> Vec<String> {
+    let file = fs::File::open(file_path).expect("Failed to open file");
+    let reader = BufReader::new(file);
+
+    let mut lines: Vec<String> = Vec::new();
+    let mut found_start_line = false;
+
+    lines.push(String::from("{"));
+    for line in reader.lines() {
+        let line = line.expect("Failed to read line");
+        let trimmed_line = line.trim();
+        
+        if trimmed_line.starts_with(start_string) {
+            found_start_line = true;
+            
+        } else if trimmed_line.starts_with(end_string) {
+            lines.push(String::from(end_string));
+            break;
+        }
+
+        if found_start_line {
+            lines.push(String::from(trimmed_line));
+        }
+
+    }
+    lines.push(String::from("}"));
+
+    lines
+}
+
+
+fn save_as_json(lines: Vec<String>, file_path: &str) {
+    let json_content = lines.join("\n");
+
+    let mut file = File::create(file_path).expect("Failed to create file");
+    file.write_all(json_content.as_bytes())
+        .expect("Failed to write to file");
+}
+
 
 fn main() {
-    rename_json("tests_export_copy", "tests_export_copy.json");
+    // rename_json("tests_export_copy", "tests_export_copy.json");
 
     let file_path = "tests_export_copy.json";
 
-    let lines = search_by_string_json(file_path, "\"payload\"");
+    // let lines = search_by_string_json(file_path, "\"messages\"");
+    // for line in lines {
+    //     println!("{}", line);
+    // }
 
-    for line in lines {
-        println!("{}", line);
-    }
+    let lines = save_block_json(file_path, "\"messages\"", "]");
+    save_as_json(lines, "modified_messages.json")
+
 }
